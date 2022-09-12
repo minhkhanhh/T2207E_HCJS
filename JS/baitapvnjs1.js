a8a14d0de5ae9c4710df39604f4dd168300b6345
minhkhanhh <102962225+minhkhanhh@users.noreply.github.com>
2022-09-12 19:09:14 +0700

    thập cẩm lú luôn

--- JS/baitapvnjs1.js
+++ JS/baitapvnjs1.js
@@ -1,30 +1,20 @@
-function loadingWeather(){
-    var xHttp = new XMLHttpRequest();
-    xHttp.onload = function (){
-        var data = JSON.parse(this.responseText)
-        var all =data.list;
-        for(var i=0;i<all.length;i++){
-            var date= all[i].dt_txt;
-            var tp = all[i].main.temp;
-            var ic = all[i].weather;
-            for(var j=0; j<ic.length;j++){
-                var image = ic[j].icon;
-                var dr = ic[j].description;
-
-                console.log(all[i].weather.icon);
-                var content = `<div class="main">
-                      <h2><span class="datetime">${date}</span></h2>
-                      <span><img src="http://openweathermap.org/img/wn/${image}@2x.png"/></span>
-                      <h1><span class = "temp">${tp}</span><sup>o</sup>C</h1>
-                      <h3><span class = "des">${dr}</span></h3>
-                      </div>`;
-                var div = document.getElementById("all");
-                div.innerHTML += content;
-            }
-
+function forecastAjax() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        var data = JSON.parse(this.responseText);
+        var list  = data.list;
+        for(var i=0;i<list.length;i++){
+            var f = `<div class="box-weather">
+                    <h4>${list[i].dt_txt}</h4>
+                    <h2>${list[i].main.temp}<sup>o</sup>C</h2>
+                    <p>${list[i].weather[0].description}</p>
+                    <img src="https://openweathermap.org/img/wn/${list[i].weather[0].icon}@2x.png" />
+                </div>` ;
+            document.getElementById("forecast").innerHTML +=f;
         }
     }
-    xHttp.open("GET","http://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric",true);
-    xHttp.send();
+    var url = "http://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    xhttp.open("GET",url,true);
+    xhttp.send();
 }
-loadingWeather();
+forecastAjax();
